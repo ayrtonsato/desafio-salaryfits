cefc4a37e240673c7c1f040a499f904a8e78f7c5
Ayrton Hiroshi Sato <ayrton_sato@hotmail.com>
2023-05-31 22:21:09 -0300

    fix: startup order problems

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,10 @@ CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
     # place your script that you only want to run on first startup.
-    ./wait-for-it.sh -t 30 mysql:3306 -- npm run deploy:db && npm run deploy:seed
+    ./wait-for-it.sh -t 30 -h mysql -p 3306 -- \
+        npx prisma migrate deploy \
+        && npx prisma db seed \
+        && npm run dev
 else
     # script that should run the rest of the times (instances where you
     # stop/restart containers).
