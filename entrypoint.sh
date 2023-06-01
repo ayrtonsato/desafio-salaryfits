63079743ba75d9b4937efebb9f49351352410701
Ayrton Hiroshi Sato <ayrton_sato@hotmail.com>
2023-05-31 21:31:55 -0300

    fix: test commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # This script checks if the container is started for the first time.
-
+set -x
 CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
     # place your script that you only want to run on first startup.
-    ./wait-for-it.sh -t 30 mysql:3306
-    npm run deploy:db && npm run deploy:seed
-    npm run dev
+    ./wait-for-it.sh -t 30 mysql:3306 -- npm run deploy:db && npm run deploy:seed
 else
     # script that should run the rest of the times (instances where you
     # stop/restart containers).
