4dcdb288164f2e9d24336d4b58234419741776e7
Ayrton Hiroshi Sato <ayrton_sato@hotmail.com>
2023-05-31 21:15:42 -0300

    fix: add wait-for-it to wait for mysql

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
     # place your script that you only want to run on first startup.
+    ./wait-for-it.sh -t 30 mysql:3306
     npm run deploy:db && npm run deploy:seed
     npm run dev
 else
